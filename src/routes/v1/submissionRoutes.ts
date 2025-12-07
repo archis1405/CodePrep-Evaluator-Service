2ebd80acf1826bcc9d2e0e7f0c6fd9355b45320c
archis1405 <archismandas1405@gmail.com>
2025-12-07 23:20:05 +0530

    Changed the file name to Zod Validator

--- src/routes/v1/submissionRoutes.ts
+++ src/routes/v1/submissionRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { addSubmission } from "../../controller/submissionController";
-import { validate } from "../../validators/createSubmissionValidator";
+import { validate } from "../../validators/zodValidator";
 import { createSubmissionZodSchema } from "../../dtos/CreateSubmissionDTO";
 
 const submissionRouter = express.Router();
